f1c3ee0159c861677d5568949b09b28bec6dd5dd
Greggy Gianini F <greggygf@gmail.com>
2018-07-15 07:45:24 +0700

    tentang greggy laundry online

---
+++ tentang_kami.php
@@ -0,0 +1,5 @@
+<h2>Tentang Kami</h2><hr color="#0263A0">
+        <p><b><br>GREGGY Laundry Online</b> adalah suatu jasa Laundry Premium secara online yang ada di Kota Malang</p><br>
+       <p> <b>GREGGY Laundry Online</b> adalah Laundry Premium yang menyediakan berbagai layanan seperti cuci,cuci kering,dan cuci kering setrika! menawarkan GRATIS pengambilan dan pengembalian!</p><br>
+       <p>Apakah Anda tidak punya <u>waktu, kemampuan, atau keinginan</u> untuk mencuci cucian anda sendiri atau anda seorang pebisnis yang tidak memiliki kapasitas untuk menangani cucian anda? di situs profesional kami akan memenuhi dan melampaui harapan anda dan membuat cucian anda yang paling berkualitas.</p><br>
+       <p><b>GREGGY Laundry Online</b> terpercaya dalam memberikan layanan laundry kualitas tertinggi yang tersedia.</p>
