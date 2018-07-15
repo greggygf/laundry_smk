77d31b36c6cfb68051e664314b03f13ed75e2e1a
Greggy Gianini F <greggygf@gmail.com>
2018-07-15 07:48:23 +0700

    index / tampilan login / tampilan pertama

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link href='http://icons.iconarchive.com/icons/iconleak/cerulean/128/science-chemistry-icon.png' rel='SHORTCUT ICON'/>
+
+</head>   
+
+ <index> 
+<link rel="stylesheet" type="text/css" href="style2.css"/>
+<body background="wallpaper-login.jpg" >
+<center>
+<font size="+3" face="Bebas Neue">GREGGY LAUNDRY ONLINE<br></font>
+<font size="2px" face="Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, sans-serif">best premium laundry in Kota Malang<br></font>
+<br><form action="cekLogin.php" method="post" class="expose">
+
+	<label for="user">Username</label> <br/>
+	<input type="text" size="25px" name="username" style="text-align:center"> <br/>
+	<label for="pass">Password</label> <br/>
+	<input type="password" size="25" name="password" id="password" style="text-align:center"><br/><br/>
+	<input type="submit" class="tombol" name="login" value="LOGIN">
+    <br><br>
+    <font size="2">Belum terdaftar? Silakan mendaftar dulu,hubungi <b>Administrator.</b></font>
+</form>
+</center>
+
+</index>
+</body>
+</html>
