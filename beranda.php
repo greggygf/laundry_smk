c214929e3fbbadf6eaee515aecc7c3a2fb0f4c30
Greggy Gianini F <greggygf@gmail.com>
2018-07-15 07:43:29 +0700

    tampilan awal (beranda)

---
+++ beranda.php
@@ -0,0 +1,6 @@
+<h2>Beranda</h2><hr color="#0263A0">
+<br>
+<video height="500" width="830" controls>
+  <source src="laundry_di_hotel.mp4" type="video/mp4">
+</video>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contoh Laundry di Hotel Grand Hermitage,Bulgaria</p>
